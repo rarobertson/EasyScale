2e6a2d1568a1c0a3fcd7e141f4b9d3423e1a7ba8
Rafaela Robertson <rrobertson@aiminspections.com>
2018-11-01 08:39:36 -0400

    project creation
    initial project

---
+++ app/src/main/java/com/easyscale/easyscale/activity/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.easyscale.easyscale.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.easyscale.easyscale.R
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Copyright 31/10/2018
+ * Rafaela Robertson
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
