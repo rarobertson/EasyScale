8b4e989fc6f55f59a5cbc5948f7e79c403c58456
Rafaela Robertson <rrobertson@aiminspections.com>
2018-12-18 14:28:35 -0500

    adda
    coomit

---
+++ app/src/main/java/com/easyscale/easyscale/base/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.easyscale.easyscale.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun setContentView(layoutResID: Int) {
+        super.setContentView(layoutResID)
+    }
+}
